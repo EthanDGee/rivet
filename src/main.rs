a6ae76104639e84548390c27f50a636c16b56dca
EthanDGee <workethangee@gmail.com>
2026-01-21 09:00:38 +0100

    feat: Created initial structure for ratatui app
    - Created stuct with sqlpath and tool_name fields
    - Created initial control flow loop for ratatui widget

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,81 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+
+pub const TOOL_NAME: &str = "rust-cli-tool";
+
+
+#[derive(Debug, Default)]
+pub struct App {
+    tool_name : String,
+    sqlpath: String,
+    exit: bool,
+}
+
+impl App {
+    pub fn new(sqlpath : String) -> Self {
+        App {
+            tool_name: TOOL_NAME.to_string(),
+            sqlpath : sqlpath,
+            exit : false
+        }
+    }
+
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+    
+    fn handle_events(&mut self) -> io::Result<()> {
+        todo!()
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(self.tool_name.clone().bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let db_info = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.sqlpath.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(db_info)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}fn main() -> io::Result<()> {
+    let mut app : App = App::new("test.sqlite3".to_string()); 
+    ratatui::run(|terminal| app.run(terminal))
 }
