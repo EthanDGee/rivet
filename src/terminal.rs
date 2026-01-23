a49f6e7ce17afa7b9324066e9ae2deebce12289b
EthanDGee <workethangee@gmail.com>
2026-01-23 07:59:45 +0100

    feat: added enter_char delete_char and minor refactoring
    history section is not moved below input operations

--- src/terminal.rs
+++ src/terminal.rs
@@ -19,12 +19,47 @@ impl Terminal {
         }
     }
 
+    // input operations
+    pub fn move_cursor_left(&mut self) {
+        if self.cursor_index == 0 {
+            return;
+        }
+        self.cursor_index -= 1
+    }
+
+    pub fn move_cursor_right(&mut self) {
+        if self.cursor_index == self.input.len() {
+            return;
+        }
+        self.cursor_index += 1
+    }
+
+    pub fn enter_char(&mut self, new_char: char) {
+        self.input.insert(self.cursor_index, new_char);
+        self.move_cursor_right();
+    }
+
+    pub fn delete_chat(&mut self) {
+        if self.cursor_index == 0 {
+            return;
+        }
+
+        // take left and right half skipping cursor_index and merge back together
+        let left_half = self.input.chars().take(self.cursor_index);
+        let right_half = self.input.chars().skip(self.cursor_index);
+        self.input = left_half.chain(right_half).collect();
+
+        self.move_cursor_left();
+    }
+
+    // history operations
     pub fn increment_history(&mut self) {
         if self.history_index == self.history.len() - 1 {
             return;
         }
 
-        // save the current_state and swap input to next        self.history[self.history_index] = self.input.clone();
+        // save the current_state and swap input to next
+        self.history[self.history_index] = self.input.clone();
         self.history_index += 1;
         self.input = self.history[self.history_index].clone();
 
@@ -43,19 +78,5 @@ impl Terminal {
 
         // move cursor to end of line
         self.cursor_index = self.input.len();
-    }
-
-    pub fn move_cursor_left(&mut self) {
-        if self.cursor_index == 0 {
-            return;
-        }
-        self.cursor_index -= 1
-    }
-
-    pub fn move_cursor_right(&mut self) {
-        if self.cursor_index == self.input.len() {
-            return;
-        }
-        self.cursor_index += 1
     }
 }
