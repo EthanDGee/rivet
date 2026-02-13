9d246078eefa4ce89328749f2a97947b5449b4fc
EthanDGee <workethangee@gmail.com>
2026-02-13 02:07:21 -0700

    refactor: Moved all action handling from app.rs to actions/mod.rs
    This is one more step closer to implementing a more maintanabel modular
    code base.

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -9,6 +9,7 @@ use crate::actions::quit_actions::QuitActions;
 use crate::actions::results_actions::ResultActions;
 use crate::actions::terminal_actions::TerminalActions;
 use crate::app::App;
+use crate::ui::screens::Screen;
 use crossterm::event::KeyEvent;
 
 pub enum Actions {
@@ -28,4 +29,19 @@ impl Default for Actions {
 
 pub trait Actionable {
     fn take_action(app: &mut App, key_event: KeyEvent);
+}
+
+impl Actions {
+    pub fn handle_actions(app: &mut App, key_event: KeyEvent) {
+        // first handle global actions
+        <GlobalActions as Actionable>::take_action(app, key_event);
+
+        // screen specific actions
+        match app.screen {
+            Screen::Terminal(_) => <TerminalActions as Actionable>::take_action(app, key_event),
+            Screen::Results(_) => <ResultActions as Actionable>::take_action(app, key_event),
+            Screen::Help(_) => <HelpActions as Actionable>::take_action(app, key_event),
+            Screen::Exiting(_) => <QuitActions as Actionable>::take_action(app, key_event),
+        }
+    }
 }
--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,9 @@
+use crate::actions::Actions;
 use crate::model::notifications::NotificationList;
 use crate::model::sql_session::SqlSession;
 use crate::ui::screens::{Screen, terminal_screen::TerminalScreen};
 use crate::ui::{themes::ColorPalette, ui};
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::event::{self, Event, KeyEventKind};
 use ratatui::{DefaultTerminal, Frame};
 use std::io;
 
@@ -34,7 +35,14 @@ impl App {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
+
+            // Handle user actions
+            match event::read()? {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    <Actions>::handle_actions(self, key_event);
+                }
+                _ => {}
+            };
 
             self.notifications.remove_expired();
 
@@ -46,113 +54,6 @@ impl App {
             }
         }
         Ok(())
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                // self.handle_global_keys(key_event);
-
-                match self.screen {
-                    // Screen::Terminal(_) => self.handle_terminal_keys(key_event),
-                    Screen::Results(_) => self.handle_results_keys(key_event),
-                    Screen::Help(_) => self.handle_help_keys(key_event),
-                    Screen::Exiting(_) => self.handle_exiting_keys(key_event),
-                    _ => {}
-                }
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    // fn handle_global_keys(&mut self, key_event: KeyEvent) {
-    //     match (key_event.code, key_event.modifiers) {
-    //         (KeyCode::Char('s'), KeyModifiers::CONTROL) => {
-    //             self.session.commit();
-    //             self.notifications
-    //                 .notify("Save", "Changes to database saved successfully.")
-    //         }
-    //         (KeyCode::Char('r'), KeyModifiers::CONTROL) => {
-    //             self.session.rollback();
-    //             self.notifications
-    //                 .notify("Rollback", "Staged changes successfully reverted.")
-    //         }
-    //         (KeyCode::Char('q'), KeyModifiers::CONTROL) => {
-    //             self.screen = Screen::Exiting(QuitScreen::new())
-    //         }
-    //         (KeyCode::Char('c'), KeyModifiers::CONTROL) => {
-    //             self.screen = Screen::Exiting(QuitScreen::new())
-    //         }
-    //         (KeyCode::Char('h'), KeyModifiers::CONTROL) => {
-    //             self.screen = Screen::Help(HelpScreen::new())
-    //         }
-    //         _ => {}
-    //     }
-    // }
-    //
-    // fn handle_terminal_keys(&mut self, key_event: KeyEvent) {
-    //     if let Screen::Terminal(terminal_screen) = &mut self.screen {
-    //         match key_event.code {
-    //             KeyCode::Up => terminal_screen.decrement_history(),
-    //             KeyCode::Down => terminal_screen.increment_history(),
-    //             KeyCode::Left => terminal_screen.move_cursor_left(),
-    //             KeyCode::Right => terminal_screen.move_cursor_right(),
-    //             KeyCode::Char(to_insert) => terminal_screen.enter_char(to_insert),
-    //             KeyCode::Backspace => terminal_screen.delete_char(),
-    //             KeyCode::Delete => {
-    //                 //TODO: resolve issues with delete turning into backspace at end of
-    //                 //line
-    //                 terminal_screen.move_cursor_right();
-    //                 terminal_screen.delete_char();
-    //             }
-    //             KeyCode::Enter => {
-    //                 if let Some(new_screen) = self.execute_command() {
-    //                     self.screen = new_screen;
-    //                 }
-    //             }
-    //             _ => {}
-    //         //         }
-    //         //     }
-    //         // }
-    //
-    //         fn handle_results_keys(&mut self, key_event: KeyEvent) {
-    //             if let Screen::Results(results_screen) = &mut self.screen {
-    //                 //handle table navigation
-    //                 if let Some(table_view) = &mut results_screen.table_view {
-    //                     match key_event.code {
-    //                         KeyCode::Char('j') | KeyCode::Down => table_view.next_row(),
-    //                         KeyCode::Char('k') | KeyCode::Up => table_view.previous_row(),
-    //                         KeyCode::Char('h') | KeyCode::Left => table_view.previous_column(),
-    //                         KeyCode::Char('l') | KeyCode::Right => table_view.next_column(),
-    //                         _ => {}
-    //                     }
-    //                 }
-    //                 // non navigation related functionality
-    //                 match key_event.code {
-    //                     KeyCode::Char('q') | KeyCode::Esc => {
-    //                         self.screen = Screen::Terminal(TerminalScreen::new());
-    //                     }
-    //                     _ => {}
-    //                 }
-    //             }
-    //         }
-
-    fn handle_help_keys(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Esc | KeyCode::Char('q') => {
-                self.screen = Screen::Terminal(TerminalScreen::new());
-            }
-            _ => {}
-        }
-    }
-
-    fn handle_exiting_keys(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('y') => self.exit(),
-            KeyCode::Char('n') => self.screen = Screen::Terminal(TerminalScreen::new()),
-            _ => {}
-        }
     }
 
     fn draw(&mut self, frame: &mut Frame) {
