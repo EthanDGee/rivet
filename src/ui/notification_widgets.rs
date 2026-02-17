85e67e53bfff590be4c209ca842bacb513e33c21
EthanDGee <workethangee@gmail.com>
2026-02-16 22:34:15 -0700

    bug: removed unnecessary return from render_notification

--- src/ui/notification_widgets.rs
+++ src/ui/notification_widgets.rs
@@ -7,8 +7,11 @@ use ratatui::widgets::{Block, Padding, Paragraph};
 pub const NOTIFICATION_WIDTH: u16 = 32;
 pub const NOTIFICATION_HEIGHT: u16 = 5;
 
-pub fn render_notification<'a>(notification: &'a Notification, theme: &'a ColorPalette) -> Paragraph<'a> {
-    return Paragraph::new(Line::from(notification.message.clone()))
+pub fn render_notification<'a>(
+    notification: &'a Notification,
+    theme: &'a ColorPalette,
+) -> Paragraph<'a> {
+    Paragraph::new(Line::from(notification.message.clone()))
         .block(
             Block::bordered()
                 .title(Line::from(notification.title.clone()).centered())
@@ -16,5 +19,5 @@ pub fn render_notification<'a>(notification: &'a Notification, theme: &'a ColorP
                 .border_style(Style::default().fg(theme.inner_border)),
         )
         .style(Style::default().fg(theme.body_text).bg(theme.background))
-        .wrap(ratatui::widgets::Wrap { trim: false });
+        .wrap(ratatui::widgets::Wrap { trim: false })
 }
